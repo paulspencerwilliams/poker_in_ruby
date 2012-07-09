5e0f19fc2af08dd84da8e4393ad0c8667b2c88c1
Paul Williams <paulswilliams@me.com>
2012-07-09 21:28:46 +0100

    empty step definitions and cucumber.yml created

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,3 +1,8 @@
+Given /^Fred has "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+
 Given /^James has "(.*?)"$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
