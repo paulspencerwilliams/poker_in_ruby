605794c7c6bee78cd654a86faab0d8f42fa0c701
Paul Williams <paulswilliams@me.com>
2012-07-09 21:27:27 +0100

    feature written, about to implement, after a little sleep at least..

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Given /^James has "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^they show hands$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the result will be "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
